a644fb472fa6c902d1b58b2b175063646e27d27d
sleepyheader <ashton.ccross@gmail.com>
2022-02-14 19:27:37 -0800

    Modified to account for the distribution of p-z on the phone keys.

--- src/edu/arapahoe/csc160/Main.java
+++ src/edu/arapahoe/csc160/Main.java
@@ -13,26 +13,44 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         System.out.println("Enter a letter:");
 
         // take string as input; make string into character. Just take the first letter.
-        char chrInput = input.next().toCharArray()[0];
+        char charInput = input.next().toCharArray()[0];
 
         // If it is a lowercase letter, make it capital to avoid dealing with lowercase AND uppercase.
-        if (chrInput > 96) {
-            chrInput -= 32;
+        if (charInput > 96) {
+            charInput -= 32;
         }
 
         int correspondingNumber = 2;
 
-        for (int numberSection = 68; numberSection < 91; numberSection += 3) {
-            if (chrInput < numberSection)  {
-                break; // found it!
+        // a - o:
+        if (charInput < 'P') {
+            for (int numberSection = 'D'; numberSection < 'P'; numberSection += 3) {
+                if (charInput < numberSection)  {
+                    break; // found it!
+                }
+                correspondingNumber++; // not here... next!
             }
-            correspondingNumber++; // not here... next!
+        }
+        // p - z:
+        else {
+            // these letters are a little more clunky, and so will be handled manually (7 has 4 letters instead of 3, 8 has 3, and 9 has 3.)
+            if (charInput < 'T') {
+                // p q r s
+                correspondingNumber = 7;
+            }
+            else if (charInput < 'W') {
+                // t u v
+                correspondingNumber = 8;
+            }
+            else {
+                // w x y z
+                correspondingNumber = 9;
+            }
         }
 
-        System.out.println("The corresponding number for " + chrInput + " is " + correspondingNumber);
+        System.out.println("The corresponding number for " + charInput + " is " + correspondingNumber);
     }
 }
